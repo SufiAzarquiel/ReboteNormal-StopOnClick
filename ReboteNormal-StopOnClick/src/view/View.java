413b02811793f006b5296dc2617cffac102ce22d
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2023-11-15 09:04:23 +0100

    Import to intellij and try a change

--- ReboteNormal-StopOnClick/src/view/View.java
+++ ReboteNormal-StopOnClick/src/view/View.java
@@ -5,11 +5,10 @@ import window.Window;
 public class View {
  
 	public static void main(String[] args) {
-	  Window v;
 	  int MAXV=3;
 	  MovementFlag flag = new MovementFlag();
 	  for (int i=0;i<=MAXV-1;i++){
-		  v=new Window(i, flag);
+		  new Window(i, flag);
 	  }
 
 	}
